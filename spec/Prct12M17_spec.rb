3ee77902889cc293c1a8ba04cd716dfa1ffb68aa
Romina Martin <rominamartin1d@gmail.com>
2014-12-18 15:18:39 +0000

    Expectativas Naranjero

--- spec/Prct12M17_spec.rb
+++ spec/Prct12M17_spec.rb
@@ -2,12 +2,38 @@ require "Prct12M17"
 require "spec_helper"
 
 describe Naranjero do
-  before :each do
+ before :each do
+   @naranjero = Naranjero.new
+ end
 
-  end
+ describe "Productor" do
+   it "# Edad 1" do
+     @naranjero.uno_mas
+     expect(@naranjero.edad).to eq(1)
+     expect(@naranjero.altura).to eq(Naranjero::VELOCIDAD_CRECIMIENTO)
+     expect(@naranjero.contador < 0).to be(true)
+   end
 
-  describe "" do
-    it "# " do
-    end
-  end
+   it "# Edad 3" do
+     @naranjero.uno_mas
+     @naranjero.uno_mas
+     @naranjero.uno_mas
+     expect(@naranjero.edad).to eq(3)
+     expect(@naranjero.altura).to eq(Naranjero::VELOCIDAD_CRECIMIENTO * 3)
+     expect(@naranjero.contador > 0).to be(true)
+   end
+ end
+
+ describe "Consumidor" do
+   it "# Edad 3 - Recoleccion" do
+     @naranjero.uno_mas
+     @naranjero.uno_mas
+     @naranjero.uno_mas
+
+     contador = @naranjero.contador
+
+     @naranjero.recolectar_una
+     expect(contador - 1).to eq(@naranjero.contador())
+   end
+ end
 end
